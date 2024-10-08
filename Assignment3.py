f486b9121ac72e9a6a548e2bae1ed3b2ac4a451a
Edwin Miyatake <122956602+edmiyatake@users.noreply.github.com>
2024-10-08 16:03:42 -0700

    finished hashing and compression function now just need to insert items and place test cases

--- Assignment3.py
+++ Assignment3.py
@@ -1,24 +1,45 @@
-SOME_RANDOM_NUMBER = 256
 
-class Item:
+import random
+
+class item:
     def __init__(self,k,v):
         self.key = k
         self.value = v
         self.next = None
 
 class hashTable:
-    def __init__(self):
-        self.defaultCapacity = SOME_RANDOM_NUMBER
+    def __init__(self, defCap, primer):
+        self.defaultCapacity = defCap
         self.size = 0
-        self.bucket = [None] * self.defaultCapacity()
+        self.bucket = [None] * defCap
+        self.a = random.randrange(primer)
+        self.b = random.randrange(primer)
+        self.p = primer
     
-    def hashing(self):
-        #goal is to add a hash function
-        exit
+    def hash(self,str):
+        #goal is to add a hash function that takes a string and returns an number
+        max = (1 << 32) - 1 
+        h = 0
+        for char in str:
+            # print(f"h << 5 is {h << 5}")
+            # print(f"h >> 27 is {h >> 27}")
+            h = ( h << 5 & max) | (h >> 27)
+            h += ord(char)
+        # print(h)
+        return h
+
+    def hashCompress(self,k):
+        # using MAD
+        # [(a+ bi)mod p]mod N
+        # p is a prime number larger than N
+        # a and b are random numbers from 0 to p - 1
+        return (self.a + self.hash(k) * self.b) % self.p % len(self.bucket)
 
     def insert(self,val):
         exit
     
     def size(self):
-        return self.size
+        return len(self.bucket)
     
+newHash = hashTable(256,12312313)
+newHash.hash("ziggy")

926ececb31d2065d8ba5a42f3a7092d6211c27d7
Edwin Miyatake <edmiyatake@gmail.com>
2024-10-21 11:29:41 -0700

    finished assignment 3

--- Assignment3.py
+++ Assignment3.py
@@ -1,14 +1,13 @@
-
-from _collections_abc import 
 import random
+import re
 
 class item:
     def __init__(self,k,v):
         self.key = k
-        self.value = v
+        self.val = v
         self.next = None
 
-class hashTable(MapBase):
+class hashTable:
     def __init__(self, defCap, primer):
         self.defaultCapacity = defCap
         self.size = 0
@@ -36,15 +35,74 @@ class hashTable(MapBase):
         # a and b are random numbers from 0 to p - 1
         return (self.a + self.hash(k) * self.b) % self.p % len(self.bucket)
 
-    def insert(self,key,val):
+    # later on, changed val to default none cause anagrams have no associted value yet
+    def insert(self,key,val=None):
         newIndex = self.hashCompress(key)
+        newItem = item(key,val)
 
-    
-    def size(self):
-        return len(self.bucket)
-    
-newHash = hashTable(256,12312313)
-print(newHash.hashCompress("ziggy"))
-print(newHash.hashCompress("edwin"))
-print(newHash.hashCompress("cool"))
+        if not self.bucket[newIndex]:
+            self.bucket[newIndex] = newItem
+        else:
+            curr = self.bucket[newIndex]
+            while curr:
+                # if key already exits, just update the value
+                if curr.key == key:
+                    curr.val = val
+                    return
+                if not curr.next:
+                    curr.next = newItem
+                    break
+                curr = curr.next
+        # always remember to update the size after inserts
+        self.size += 1
 
+    def getSize(self):
+        return self.size
+    
+    def get(self,key):
+        newIndex = self.hashCompress(key)
+        curr = self.bucket[newIndex]
+        while curr:
+            if curr.key == key:
+                return curr.val
+            curr = curr.next
+        return -1
+    
+    def printTable(self):
+        for i, item in enumerate(self.bucket):
+            print(f"Index {i}:", end=" ")
+            curr = item
+            while curr:
+                print(f"({curr.key}, {curr.val})", end=" -> ")
+                curr = curr.next
+            print("None")
+
+def countAnagram():
+    newHash = hashTable(1000,7919)
+    with open("pride-and-prejudice.txt",'r') as file:
+        for line in file:
+            words = re.split(r"[^a-zA-z0-9]",line)
+            for word in words:
+                anagram = ''.join(sorted(word.lower()))
+                newHash.insert(anagram)
+
+    return newHash.getSize()
+
+# newHash = hashTable(256,12312313)
+# print(newHash.hashCompress("ziggy"))
+# print(newHash.hashCompress("edwin"))
+# print(newHash.hashCompress("cool"))
+
+# picked a random prime from wikipedia
+# newMap = hashTable(100,7919)
+
+# Insert test cases (key-value pairs)
+# newMap.insert("apple", 100)
+# newMap.insert("banana", 200)
+# newMap.insert("grape", 300)
+# newMap.insert("orange", 400)
+# newMap.insert("melon", 500)
+
+# newMap.getSize()
+# newMap.printTable()
+# print(countAnagram())
